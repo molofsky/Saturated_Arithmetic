2bdc623aa5e60e8816b3e3dc4aa835054b6e8ca4
molofsky <114968002+molofsky@users.noreply.github.com>
2022-12-26 09:31:38 -0800

    Update saturate.c

--- saturate.c
+++ saturate.c
@@ -1,19 +1,28 @@
+/* A function to saturate arithmetic for n bit numbers
+* 
+* @author Adrian Molofsky
+* @version 12/26/22
+*/
+
 #include <stdio.h> 
 #include <stdlib.h>
 
-
-
 /* Function to saturate arithmetic to be used
 * in main. Saturates arithmetic if the input is greater than 
-* the 16 bit max 2^16 - 1.
+* the n bit max 2^n - 1.
 */
-size_t saturate(size_t n) {
+size_t saturate(size_t n, int d) {
 
   return n;
 }
 
+
+/* main calls the saturate function with user input */
 int main() {
   printf("What number would you like to saturate?");
-  printf("The number %lu saturated is %lu"
+  
+  printf("For what bit max would you like to saturate?");
+  
+  printf("The number %lu saturated for %d is %lu")
   return 0;
 }
